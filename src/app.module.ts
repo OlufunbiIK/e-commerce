032c8117cf5b122c442175696897e04e17e775d4
OlufunbiIK <olufunbiibrahim@gmail.com>
2025-02-11 18:23:56 +0100

    env config in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'; //step 1 for env variables
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env.development'] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD' as string),
+        database: configService.get('POSTGRES_NAME'),
+        synchronize: configService.get('POSTGRES_SYNC'),
+        autoLoadEntities: configService.get('DATABASE_LOAD'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
