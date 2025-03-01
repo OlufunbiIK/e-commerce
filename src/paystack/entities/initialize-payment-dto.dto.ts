469bb50bf6d49ca74768d2b319ccf405981d7acd
OlufunbiIK <olufunbiibrahim@gmail.com>
2025-03-01 19:46:29 +0100

    Added swagger documentation for paystack resource

--- src/paystack/entities/initialize-payment-dto.dto.ts
+++ src/paystack/entities/initialize-payment-dto.dto.ts
@@ -1,10 +1,21 @@
 import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class InitializePaymentDto {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'The email of the user making the payment',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: 5000,
+    description: 'Amount to be paid (in kobo for NGN currency)',
+    required: true,
+  })
   @IsNumber()
   @IsNotEmpty()
   amount: number;
