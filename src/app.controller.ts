a8a44b279cd6cdbd3f59cf4c4e4d734868f79aa5
OlufunbiIK <olufunbiibrahim@gmail.com>
2025-03-01 20:36:25 +0100

    Improved Group ReadMe file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,16 +2,20 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@ApiTags('App') // Groups this controller under "App" in Swagger
+@ApiTags('Welcome') // Groups this controller under "Welcome" in Swagger UI
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @ApiOperation({
-    summary: 'Get Welcome Message',
-    description: 'Returns a simple welcome message from the server.',
+    summary: 'Welcome to Our E-Commerce API',
+    description:
+      'This endpoint returns a friendly welcome message, introducing users to our powerful and feature-rich eCommerce API.',
   })
-  @ApiResponse({ status: 200, description: 'Successfully retrieved message' })
+  @ApiResponse({
+    status: 200,
+    description: 'Welcome message successfully retrieved.',
+  })
   @Get()
   getHello(): string {
     return this.appService.getHello();
