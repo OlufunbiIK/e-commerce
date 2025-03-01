c7bd574d91fc2a907ccb0a2e58f2803c264ea437
OlufunbiIK <olufunbiibrahim@gmail.com>
2025-03-01 20:00:05 +0100

    Added swagger documentation for order resource

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -13,27 +14,56 @@ import { OrderStatus } from '../enum/orderStatus.enum';
 
 @Entity()
 export class Order {
+  @ApiProperty({
+    description: 'Unique identifier for the order',
+    example: 1,
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    description: 'User who placed the order',
+    type: () => User,
+  })
   @ManyToOne(() => User, (user) => user.orders, {
     eager: true,
     onDelete: 'CASCADE',
   })
   user: User;
 
+  @ApiProperty({
+    description: 'List of items in the order',
+    type: () => [OrderItem], // Array of OrderItem objects
+  })
   @OneToMany(() => OrderItem, (orderItem) => orderItem.order, { cascade: true })
   orderItems: OrderItem[];
 
+  @ApiProperty({
+    description: 'Total price of the order',
+    example: 299.99,
+  })
   @Column('decimal', { precision: 10, scale: 2 })
   totalPrice: number;
 
+  @ApiProperty({
+    description: 'Current status of the order',
+    enum: OrderStatus,
+    example: OrderStatus.PENDING,
+  })
   @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.PENDING })
   status: OrderStatus;
 
+  @ApiProperty({
+    description: 'Timestamp when the order was created',
+    example: '2024-03-01T12:34:56.789Z',
+  })
   @CreateDateColumn()
   createdAt: Date;
 
+  @ApiProperty({
+    description: 'Timestamp when the order was last updated',
+    example: '2024-03-02T14:45:30.123Z',
+  })
   @UpdateDateColumn()
   updatedAt: Date;
 }
