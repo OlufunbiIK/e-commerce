737c501011d837a91e35cf5d74438b048562b0b8
OlufunbiIK <olufunbiibrahim@gmail.com>
2025-03-01 19:22:52 +0100

    Added swagger documentation for users resource

--- src/user/dto/get-users-dto.dto.ts
+++ src/user/dto/get-users-dto.dto.ts
@@ -1,13 +1,22 @@
-/* eslint-disable prettier/prettier */
 import { IsDate, IsOptional } from 'class-validator';
-import { IntersectionType } from '@nestjs/swagger';
+import { IntersectionType, ApiProperty } from '@nestjs/swagger';
 import { PaginationQueryDto } from 'src/common/interceptors/pagination/pagination-query-dto.dto';
 
 class GetUsersBaseDto {
+  @ApiProperty({
+    example: '2024-01-01',
+    description: 'Filter users from this start date',
+    required: false,
+  })
   @IsDate()
   @IsOptional()
   startDate?: Date;
 
+  @ApiProperty({
+    example: '2024-12-31',
+    description: 'Filter users until this end date',
+    required: false,
+  })
   @IsDate()
   @IsOptional()
   endDate?: Date;
